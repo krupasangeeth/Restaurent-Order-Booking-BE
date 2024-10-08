75fccd59fb65a80e63d0ebb0cd560f8491e0028e
Mahmmed-Ilieyaz <mahmmedilieyaz@gmail.com>
2024-10-09 00:20:40 +0530

    added swagger dependency

--- food/src/main/resources/import.sql
+++ food/src/main/resources/import.sql
@@ -1,11 +1,11 @@
-INSERT INTO menu_item (menu_item_id,price,category,name) values (10001,250,'veg','Veg Biryani');
-INSERT INTO menu_item (menu_item_id,price,category,name) values (10002,250,'veg','Veg Pulav');
-INSERT INTO menu_item (menu_item_id,price,category,name) values (3,350,'non-veg','Chicken Biryani');
-INSERT INTO menu_item (menu_item_id,price,category,name) values (4,450,'non-veg','Mutton Biryani');
-INSERT INTO menu_item (menu_item_id,price,category,name) values (5,550,'non-veg','Prwans Biryani');
-INSERT INTO menu_item (menu_item_id,price,category,name) values (6,250,'non-veg','Fish Biryani');
-INSERT INTO menu_item (menu_item_id,price,category,name) values (7,850,'non-veg','Crab Biryani');
-INSERT INTO menu_item (menu_item_id,price,category,name) values (8,50,'non-veg','Dog Biryani');
+INSERT INTO menu_item (menu_item_id,price,category,name) values (10001,250,'VEG','Veg Biryani');
+INSERT INTO menu_item (menu_item_id,price,category,name) values (10002,250,'VEG','Veg Pulav');
+INSERT INTO menu_item (menu_item_id,price,category,name) values (3,350,'NON_VEG','Chicken Biryani');
+INSERT INTO menu_item (menu_item_id,price,category,name) values (4,450,'NON_VEG','Mutton Biryani');
+INSERT INTO menu_item (menu_item_id,price,category,name) values (5,550,'NON_VEG','Prwans Biryani');
+INSERT INTO menu_item (menu_item_id,price,category,name) values (6,250,'NON_VEG','Fish Biryani');
+INSERT INTO menu_item (menu_item_id,price,category,name) values (7,850,'NON_VEG','Crab Biryani');
+INSERT INTO menu_item (menu_item_id,price,category,name) values (8,50,'NON_VEG','Dog Biryani');
 
 INSERT INTO USER_ROLE (ROLE_ID,ROLE) Values (10001,'ADMIN')
 INSERT INTO USER_ROLE (ROLE_ID,ROLE) Values (10002,'USER')
